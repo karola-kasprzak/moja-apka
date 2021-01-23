e3e5b0a0645c692f05d60832f2f2ff1efac0599d
karola-kasprzak <karola.a.kasprzak@gmail.com>
2021-01-23 15:02:06 +0100

    added dummy data

--- src/Components/Crypto/Crypto.js
+++ src/Components/Crypto/Crypto.js
@@ -6,7 +6,169 @@ class Crypto extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            cryptoData: null,
+            cryptoData: {
+                USD: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "$",
+                },
+                AUD: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "$",
+                },
+                BRL: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "R$",
+                },
+                CAD: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "$",
+                },
+                CHF: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "CHF",
+                },
+                CLP: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "$",
+                },
+                CNY: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "¥",
+                },
+                DKK: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "kr",
+                },
+                EUR: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "€",
+                },
+                GBP: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "£",
+                },
+                HKD: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "$",
+                },
+                INR: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "₹",
+                },
+                ISK: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "kr",
+                },
+                JPY: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "¥",
+                },
+                KRW: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "₩",
+                },
+                NZD: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "$",
+                },
+                PLN: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "zł",
+                },
+                RUB: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "RUB",
+                },
+                SEK: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "kr",
+                },
+                SGD: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "$",
+                },
+                THB: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "฿",
+                },
+                TRY: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "₺",
+                },
+                TWD: {
+                    "0m": 0,
+                    last: 0,
+                    buy: 0,
+                    sell: 0,
+                    symbol: "NT$",
+                },
+            },
             currencyList: null,
             staticCurrencyList: [
                 "USD",
@@ -70,14 +232,18 @@ class Crypto extends Component {
         return (
             <div>
                 <p>All currencies: {currencyList.join(" ")}</p>
-                {/* <CryptoList currency="TEST" buy="0" sell="0" /> */}
+                <CryptoList
+                    currency="TEST USD"
+                    buy={this.state.cryptoData.USD.buy}
+                    sell={this.state.cryptoData.USD.sell}
+                />
 
                 {currencyList.map((item, index) => (
                     <CryptoList
                         key={index}
                         currency={item}
                         // nie dziala poprawnie
-                        // buy={this.state.cryptoData.item.buy}
+                        // buy={this.state.cryptoData.{item}.buy}
                         // sell={this.state.cryptoData.item.sell}
                     />
                 ))}
