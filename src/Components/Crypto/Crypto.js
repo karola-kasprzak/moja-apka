005490280b4c8d56e4d26a75fee16b3ac4f622ec
karola-kasprzak <karola.a.kasprzak@gmail.com>
2021-01-24 14:22:55 +0100

    fixed crypto logic

--- src/Components/Crypto/Crypto.js
+++ src/Components/Crypto/Crypto.js
@@ -6,216 +6,46 @@ class Crypto extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            cryptoData: {
-                USD: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "$",
-                },
-                AUD: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "$",
-                },
-                BRL: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "R$",
-                },
-                CAD: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "$",
-                },
-                CHF: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "CHF",
-                },
-                CLP: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "$",
-                },
-                CNY: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "¥",
-                },
-                DKK: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "kr",
-                },
-                EUR: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "€",
-                },
-                GBP: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "£",
-                },
-                HKD: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "$",
-                },
-                INR: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "₹",
-                },
-                ISK: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "kr",
-                },
-                JPY: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "¥",
-                },
-                KRW: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "₩",
-                },
-                NZD: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "$",
-                },
-                PLN: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "zł",
-                },
-                RUB: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "RUB",
-                },
-                SEK: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "kr",
-                },
-                SGD: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "$",
-                },
-                THB: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "฿",
-                },
-                TRY: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "₺",
-                },
-                TWD: {
-                    "0m": 0,
-                    last: 0,
-                    buy: 0,
-                    sell: 0,
-                    symbol: "NT$",
-                },
-            },
-            currencyList: null,
-            staticCurrencyList: [
-                "USD",
-                "AUD",
-                "BRL",
-                "CAD",
-                "CHF",
-                "CLP",
-                "CNY",
-                "DKK",
-                "EUR",
-                "GBP",
-                "HKD",
-                "INR",
-                "ISK",
-                "JPY",
-                "KRW",
-                "NZD",
-                "PLN",
-                "RUB",
-                "SEK",
-                "SGD",
-                "THB",
-                "TRY",
-                "TWD",
-            ],
+            cryptoData: [],
+            currencyList: [],
+            staticCurrencyList: [],
         };
     }
 
     getCrypto = () => {
-        return axios
-            .get("https://blockchain.info/pl/ticker")
-            .then((response) => {
-                this.setState({
-                    cryptoData: response.data,
-                    currencyList: Object.keys(response.data),
-                });
-            })
-            .catch((err) => console.log(err))
-            .then(console.log(this.state.cryptoData));
+        return (
+            axios
+                .get("https://blockchain.info/pl/ticker")
+                .then((response) => {
+                    const cryptoRes = response.data;
+                    let cryptoArr = [];
+
+                    cryptoArr = Object.keys(cryptoRes).map((key) => {
+                        return {
+                            currency: key,
+                            buy: cryptoRes[key].buy,
+                            sell: cryptoRes[key].sell,
+                            symbol: cryptoRes[key].symbol,
+                            lastRate: cryptoRes[key].last,
+                        };
+                    });
+                    // console.log(cryptoArr);
+
+                    this.setState({
+                        cryptoData: cryptoArr,
+                        currencyList: Object.keys(cryptoRes),
+                    });
+                })
+                // .catch((err) => console.log(`Error: ${err}`))
+                .then(console.log(this.state.cryptoData))
+        );
     };
 
     componentDidMount() {
         // Call this function so that it fetch first time right after mounting the component
         this.getCrypto();
-        // set Interval
-        this.interval = setInterval(this.getCrypto, 5000);
+        // set Interval - set to 30s
+        this.interval = setInterval(this.getCrypto, 30000);
     }
 
     componentWillUnmount() {
@@ -224,29 +54,13 @@ class Crypto extends Component {
     }
 
     render() {
-        const currencyList =
-            this.state.currencyList === null
-                ? this.state.staticCurrencyList
-                : this.state.currencyList;
+        const currencyList = this.state.currencyList.join(" ");
 
         return (
             <div>
-                <p>All currencies: {currencyList.join(" ")}</p>
-                <CryptoList
-                    currency="TEST USD"
-                    buy={this.state.cryptoData.USD.buy}
-                    sell={this.state.cryptoData.USD.sell}
-                />
-
-                {currencyList.map((item, index) => (
-                    <CryptoList
-                        key={index}
-                        currency={item}
-                        // nie dziala poprawnie
-                        // buy={this.state.cryptoData.{item}.buy}
-                        // sell={this.state.cryptoData.item.sell}
-                    />
-                ))}
+                <h1>BitCoin Rates</h1>
+                <p>Tracked currencies: {currencyList}</p>
+                <CryptoList rates={this.state.cryptoData} />
             </div>
         );
     }
--- src/Components/Crypto/CryptoList/CryptoList.js
+++ src/Components/Crypto/CryptoList/CryptoList.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 function CryptoRate(props) {
-    return (
-        <div>
-            <p>
-                {props.currency} Buy: {props.buy} Sell: {props.sell}
+    let cryptoList = props.rates.map((crypto) => {
+        return (
+            <p key={crypto.currency} className="CryptoList-ListItem">
+                {crypto.currency} Buy: {crypto.buy} Sell: {crypto.sell}
             </p>
+        );
+    });
+
+    return (
+        <div className="CryptoList-Container">
+            <div className="CryptoList-List"> {cryptoList} </div>
         </div>
     );
 }
